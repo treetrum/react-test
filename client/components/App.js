cdf493a2f69f41fff688758027ac9561977b2577
TreeTrum <sam@sjd.co>
2018-05-08 10:13:02 +1000

    Put styles import in a better location

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import '../styles/main.scss';
-
 class App extends React.Component {
     render() {
         return (
--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './styles/main.scss';
+
 import App from './components/App.js'
 
 ReactDOM.render(
